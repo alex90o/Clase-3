1ffeceefd99c06343aef93646b896ffca1a2f7fa
Alexis <alex10o@hotmail.com>
2023-03-06 19:04:10 -0300

    se realizo el alret y el prompt

--- Proyecto_clase/js/app.js
+++ Proyecto_clase/js/app.js
@@ -2,3 +2,12 @@ var a = 120;
 var b =550;
 
 console.log(a+b);
+
+alert(" ♥♥♥ Saludar ♦♦♦")
+ 
+
+ var nombre = prompt("Ingrese su nombre")
+
+ var apellido = prompt("Ingrese su apellido")
+
+ alert("Hola " + nombre +" "+ apellido + " bienvenido que tengas un buen día"); 
